8b7e7ace1e98bee4b70ac1e96d033ff0f02a1aeb
Mohsen Bazmi <55460935+Mohsen-Bazmi@users.noreply.github.com>
2023-01-20 12:30:38 +0330

    Add the `RecepieController`

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudInMvc.Models;
+
+public class RecipeController : Controller
+{
+    public ActionResult List()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ActionResult FindById(Guid recipeId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ActionResult Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ActionResult Update(Guid recipeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ActionResult Remove(Guid recipeId)
+    {
+        throw new NotImplementedException();
+    }
+}
