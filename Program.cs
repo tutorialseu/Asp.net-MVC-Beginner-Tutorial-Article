1563678ecf45d1fcfb8e39b8b09ef756ece9f507
Mohsen Bazmi <55460935+Mohsen-Bazmi@users.noreply.github.com>
2023-01-23 22:24:44 +0330

    Configure the default route

--- Program.cs
+++ Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Recipe}/{action=List}");
 
 app.Run();
